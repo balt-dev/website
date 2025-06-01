5e3b43991281741763a5d2b4cad41ceca252d1cf
balt-dev <heptor42+git@gmail.com>
2025-06-01 16:33:40 -0500

    Add responsivity and 404

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function createShader(gl, type, source) {
         return shader;
     }
     
-    alert(gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
 }
 function createProgram(gl, vertexShader, fragmentShader) {
@@ -15,17 +14,14 @@ function createProgram(gl, vertexShader, fragmentShader) {
     gl.attachShader(program, vertexShader);
     try {
         gl.attachShader(program, fragmentShader);
-    } catch (err) {
-        alert("Fragment shader failed!\n" + err.message);
-    }
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
+        gl.linkProgram(program);
+        var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+        if (success) {
+            return program;
+        }
 
-    alert(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
+        gl.deleteProgram(program);
+    } catch (err) {}
 }
 function fixCanvas() { // This isn't perfect but I really don't need that
     const canvas = document.querySelector("#background");
@@ -52,36 +48,11 @@ const gl = canvas.getContext( 'webgl', { antialias: false, depth: false, stencil
 var program;
 var positionAttributeLocation;
 var positionBuffer;	
-function init() {
 
-    if (!gl) {
-        alert("WebGL not initialized! Check your browser's compatibility.\nDisabling background shader.");
-        return;
-    }
 
-    let frag = createShader(gl, gl.FRAGMENT_SHADER, document.querySelector("#frag").text);
-    let vert = createShader(gl, gl.VERTEX_SHADER, document.querySelector("#vert").text);
-    program = createProgram(gl, vert, frag);
-
-    positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // three 2d points
-    const positions = [
-        -1, -1,
-        -1,  1,
-        1, -1,
-        1,  1,
-        -1,  1,
-        1, -1,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-};
-init();
 function draw() {
     if (!gl) return;
-	let dayProgress = (((Date.now()) + 43200000) % 86400000) / 86400000; // this is specifically in MY timezone
+    let dayProgress = (((Date.now()) + 43200000) % 86400000) / 86400000; // this is specifically in MY timezone
     let viewportResolutionLocation = gl.getUniformLocation(program, "resolution");
     let elapsedTimeLocation = gl.getUniformLocation(program, "time");
     let dayProgressLocation = gl.getUniformLocation(program, "dayProgress");
@@ -112,7 +83,35 @@ function draw() {
         positionAttributeLocation, size, type, normalize, stride, offset);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 };
-let drawLoop = setInterval(draw, 17 );
+
+function init() {
+
+    if (!gl) {
+        alert("WebGL not initialized! Check your browser's compatibility.\nDisabling background shader.");
+        return;
+    }
+
+    let frag = createShader(gl, gl.FRAGMENT_SHADER, document.querySelector("#frag").text);
+    let vert = createShader(gl, gl.VERTEX_SHADER, document.querySelector("#vert").text);
+    program = createProgram(gl, vert, frag);
+
+    positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // three 2d points
+    const positions = [
+        -1, -1,
+        -1,  1,
+        1, -1,
+        1,  1,
+        -1,  1,
+        1, -1,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    setInterval(draw, 17);
+};
+init();
 
 function handleAnchor(new_anchor) {
     let anchor = (typeof(new_anchor) === "string") ? new_anchor : window.location.hash.substring(1); // Get the anchor from the URL
